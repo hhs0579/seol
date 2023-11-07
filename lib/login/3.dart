8bf89bb3491b74a8323fd6150328fa056015358b
hhs0579 <hhs0579@naver.com>
2023-11-07 17:32:56 +0900

    dd

--- lib/login/3.dart
+++ lib/login/3.dart
@@ -13,9 +13,68 @@ class login3 extends StatelessWidget {
         padding: const EdgeInsets.all(40),
         height: height,
         width: width,
-        child: Column(children: [
-          
-        ]),
+        child: SingleChildScrollView(
+          child: Column(children: [
+            Row(
+              children: [
+                Container(
+                  width: 15,
+                  height: 15,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff7C7C7C),
+                      border:
+                          Border.all(color: const Color(0xff7C7C7C), width: 1),
+                      borderRadius: BorderRadius.circular(100)),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  width: 15,
+                  height: 15,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border:
+                          Border.all(color: const Color(0xff7C7C7C), width: 1),
+                      borderRadius: BorderRadius.circular(100)),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  width: 15,
+                  height: 15,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border:
+                          Border.all(color: const Color(0xff7C7C7C), width: 1),
+                      borderRadius: BorderRadius.circular(100)),
+                )
+              ],
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    '앱에서 사용할',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  Text(
+                    '별명을 정해주세요',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+                  )
+                ],
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
